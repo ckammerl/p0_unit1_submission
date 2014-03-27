f7e8742b97793bd2651ec19f1809d612b0295df1
Christiane Kammerl <christiane@kammerl.com>
2014-03-27 11:39:23 -0700

    added solution

--- 4_median_solo_challenge/my_solution.rb
+++ 4_median_solo_challenge/my_solution.rb
@@ -4,22 +4,136 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? array of integers or array of strings
+# What is the output? (i.e. What should the code return?) return the median of the array as a integer / a string resp.
 # What are the steps needed to solve the problem?
+=begin
+1. define a method median which takes an array as input 
+2. sort the elements of your array in ascending order
 
+IF array is an array of numbers
+
+
+   		IF the the number of elements in your array is odd
+   			THEN get 'x' as the index of the median by dividing the array's (length - 1) by 2
+   			 	 set median equal to array value at index 'x'
+		ELSE
+			THEN get 'y' as the index of the number next to the median by dividing the array's (length) by 2
+			 	 get 'm' as the value of the median as a float by summing the value of the array at index 'y' with the value of the array at index ('y' - 1) and 
+				 dividing the result by 2.0 
+   			 	 set median equal to 'm'
+		END
+
+ELSIF array is an array of strings AND array.length is odd
+	THEN get 'z' as the index of the median by dividing the array's (length - 1) by 2
+		 set median equal to the array value at index 'z'
+
+ELSE
+	puts message "Error: The input must be either 
+					a) array of numbers with an even or odd number of elements or 
+					b) an array of strings with an odd number of elements."
+
+END
+
+3. return median
+
+=end
 
 # 2. Initial Solution
 
+def median(source)
+	source = source.sort
 
+	if source.all? { |element| element.is_a? Integer }
+
+		if source.length % 2 != 0
+			x = (source.length - 1) / 2
+			median = source[x]
+
+		else
+			y = source.length / 2
+			m = (source[y] + source[y - 1]) /2.0
+			median = m
+		end
+
+	elsif source.all? { |element| element.is_a? String } && (source.length % 2 != 0)
+
+			z = (source.length - 1) / 2
+			median = source[z]
+
+
+	else
+		puts "Error: The input must be either a) array of numbers with an even or odd number of elements or b) an array of strings with an odd number of elements."
+	end
+  median
+end
 
 
 # 3. Refactored Solution
 
+def median(source)
+	if source.all? { |element| element.is_a? Integer }
+	   source = source.sort
+		if source.length % 2 != 0
+			index_median = (source.length - 1) / 2 
+			median = source[index_median]
+
+		else
+			y = source.length / 2 # y is index of element next to median
+			value_median = (source[y] + source[y - 1]).fdiv(2) # calling method .fdiv to receive float
+			median = value_median
+		end
+
+	elsif source.all? { |element| element.is_a? String } && (source.length % 2 != 0)
+		  	source = source.sort_by { |element| element.downcase } # sorting by key element.downcase
+		  	index_median = (source.length - 1) / 2 
+			median = source[index_median]
+
+		else
+			puts "Error: The input must be either\n a) array of integers with an even or odd number of elements or\n b) an array of strings with an odd number of elements."
+	end
+  return median
+end
 
 
 # 4. Reflection 
+
+=begin
+What parts of your strategy worked? What problems did you face?
+I had to look up methods to check if the source was an array of integers or an array of strings
+
+What questions did you have while coding? What resources did you find to help you answer them?
+check if element of array is instance of certain class with .all? {block} http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-all-3F
+division by float with .fdiv(): http://www.ruby-doc.org/core-2.1.0/Fixnum.html#method-i-fdiv
+
+How confident are you with each of the learning objectives?
+confident
+
+Which parts of the challenge did you enjoy?
+repeating the strategy to define the median and strenghtening the knowledge
+
+Which parts of the challenge did you find tedious?
+I can't think of an use case when defining the median on an array of strings with an even number of elements makes sense
+
+
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 5_pad_array/my_solution.rb
+++ 5_pad_array/my_solution.rb
@@ -1,27 +1,127 @@
 # U1.W3: Pad an Array!
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # Complete each step below according to the challenge directions and 
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
-
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? array (self), min size and optional pad value
+# What is the output? (i.e. What should the code return?) pad returns a copy of self; pad! returns a modified (padded) self
 # What are the steps needed to solve the problem?
+=begin
+
+WRITE a the Array method .pad! which takes an integer as minimum value (min) and an default pad value (x) equal to nil as arguments
+SET length equal to self.length - self is an instance of Array on which you can call on .pad!
+	IF self.length is less than min
+		set a variable (y) equal to result of substracting self.length from min
+		y times do push x into self
+	ELSE
+		return self
+	END
+
+WRITE the Array method .pad which behaves identically to the method .pad! but will will return a modified copy of self
+INITIALIZE a new object by cloning self
+	SET length equal to cloned object's length
+	IF clone.length is less than min
+		set a variable (y) equal to result of substracting clone.length from min
+		y times do push x into clone
+	ELSE
+		return clone
+	END
+	
+=end
 
 
 # 2. Initial Solution
 
+class Array 
+	def pad!(min, x = nil)
+		len = self.length
+		if len < min
+			pad_num = (min - len)
+			pad_num.times do self.push(x) end
+			self 
+		
+		else 
+			self
+		end 
+	end
+
+	def pad(min, x = nil)
+		padded_arr = self.clone
+		length = padded_arr.length
+		if length < min
+			pad_num = (min - padded_arr.length)
+			pad_num.times do padded_arr.push(x) end
+			padded_arr
+		else 
+			padded_arr
+		end 
+	end
+end
 
 
 
 # 3. Refactored Solution
 
 
+	class Array 
+		def pad!(min, x = nil)
+		len = self.length
+		if len < min
+			pad_num = (min - len)
+			pad_num.times do self.push(x) end
+		end 
+		return self
+	end
+
+
+	def pad(min, x = nil)	
+		padded_arr = self.clone
+		padded_arr.pad!(min, x)
+		return padded_arr
+	end
+end
+
 
 # 4. Reflection 
+=begin
+What parts of your strategy worked? What problems did you face?
+Writing the pad and pad! methods with the correct parameters and if statement worked well; I had to look up how to define a method as
+Array#method and the method .clone()
+
+What resources did you find to help you answer them?
+http://alancohen.tumblr.com/post/31078056901/learning-to-code-the-hard-way-destructive-vs
+http://ruby-doc.org/core-1.9.3/Object.html#method-i-clone
+http://stackoverflow.com/questions/17858816/ruby-adding-new-method-to-array-class
+
+Did you learn any new skills or tricks?
+I learned how to use self as object in my method; how to clone an object in order to write a non-destructive method and to call a method within a method
+
+How confident are you with each of the learning objectives?
+Use tests to guide development - confident; this challenge clearly demonstrated why tests should be written first; they check the method in all possible ways and help
+to come up with a pointed, DRY solution at the end 
+Break down problems into implementable pseudocode - confident
+Differentiate between and produce destructive and non-destructive methods - confident
+Find and use built-in Ruby methods to solve challenges - confident
+
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
